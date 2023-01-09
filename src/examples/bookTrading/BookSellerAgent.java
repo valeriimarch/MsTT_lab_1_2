b748c563976efddeb9b35e172cf401dcfa51f649
SergioIvashyn <45632379+SergioIvashyn@users.noreply.github.com>
2023-01-09 18:57:31 +0200

    Init
    Co-Authored-By: valeriimarch <122287371+valeriimarch@users.noreply.github.com>

---
+++ src/examples/bookTrading/BookSellerAgent.java
@@ -0,0 +1,99 @@
+package examples.bookTrading;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.*;
+
+public class BookSellerAgent extends Agent {
+    private Hashtable catalogue;
+    private BookSellerGui myGui;
+
+    protected void setup() {
+        catalogue = new Hashtable();
+        myGui = new BookSellerGui(this);
+        myGui.showGUI();
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("book-selling");
+        sd.setName("JADE-book-trading");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        addBehaviour(new OfferRequestsServer());
+        addBehaviour(new PurchaseOrdersServer());
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        myGui.dispose();
+        System.out.println("Seller agent " + getAID().getName() + " terminating.");
+    }
+
+    public void updateCatalogue(final String title, final int price) {
+        addBehaviour(new OneShotBehaviour() {
+            @Override
+            public void action() {
+                catalogue.put(title, new Integer(price));
+            }
+        });
+    }
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            ACLMessage msg = myAgent.receive();
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = (Integer) catalogue.get(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price.intValue()));
+                } else {
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                Integer price = (Integer) catalogue.get(title);
+                if (price != null) {
+                    reply.setPerformative(ACLMessage.INFORM);
+                    System.out.println(title + " sold to agent " + msg.getSender().getName());
+                } else {
+                    reply.setPerformative(ACLMessage.FAILURE);
+                    System.out.println("not-available");
+                }
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+    }
+}
